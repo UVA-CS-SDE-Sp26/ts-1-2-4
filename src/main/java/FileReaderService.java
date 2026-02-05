995bf93cf94c348e4b4862674843f6c5502d6834
Tonycs <lam.tony540@gmail.com>
2026-02-05 14:42:31 -0500

    Created readFile method

--- src/main/java/FileReaderService.java
+++ src/main/java/FileReaderService.java
@@ -1,2 +1,20 @@
+import java.io.*;
+
 public class FileReaderService {
+    public String readFile(String path) throws IOException {
+        File f = new File(path);
+        if (f.exists()) {
+            BufferedReader br = new BufferedReader(new FileReader(f));
+            StringBuilder content = new StringBuilder();
+
+            String line;
+            while ((line = br.readLine()) != null) {
+                content.append(line);
+            }
+
+            return content.toString();
+        } else {
+            return "File does not exist";
+        }
+    }
 }
