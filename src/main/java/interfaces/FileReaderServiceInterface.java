a7646c84115833d26df65dfd1417d843dc7c91fa
Tonycs <lam.tony540@gmail.com>
2026-02-07 15:00:05 -0500

    Added readFile to file reader interface

--- src/main/java/interfaces/FileReaderServiceInterface.java
+++ src/main/java/interfaces/FileReaderServiceInterface.java
@@ -1,4 +1,12 @@
 package interfaces;
+import java.io.IOException;
 
 public interface FileReaderServiceInterface {
+    /**
+     * Reads the content of a file at the given path.
+     * @param path The absolute or relative path to the file.
+     * @return The content of the file as a String.
+     * @throws IOException If the file cannot be read.
+     */
+    String readFile(String path) throws IOException;
 }
